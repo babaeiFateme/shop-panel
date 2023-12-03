ff6f81796b8f140e62d428477836d56f263ba021
fatemeh <babaei.f1996@gmail.com>
2023-12-03 10:30:40 +0330

    fix: profile

--- src/components/partials/DashbardLayout/Components/DDashboardHeader/DDashboardHeader.jsx
+++ src/components/partials/DashbardLayout/Components/DDashboardHeader/DDashboardHeader.jsx
@@ -11,7 +11,6 @@ const DDashboardHeader = () => {
         <div className='bg-primary-50 flex justify-between items-center min-h-[60px] p-6'>
             <div className='flex gap-5 items-center'>
                 <div className='flex items-center gap-2'>
-                    <Image src={avatar} width={50} height={50} />
                     <div className='flex flex-col gap-1 font-semibold text-sm'>
                         <span>Fateme Babaei</span>
                         <span className='text-gray-500'>Admin</span>
--- src/components/partials/DashbardLayout/Components/DDashboardSidebar/DDashboardSidebar.jsx
+++ src/components/partials/DashbardLayout/Components/DDashboardSidebar/DDashboardSidebar.jsx
@@ -1,10 +1,27 @@
 'use client'
-import { BiSolidDashboard, BiSolidGroup, BiSolidTv,BiSolidUser } from 'react-icons/bi'
+import { profileHttp } from '@core/services/api'
+import { BiSolidDashboard, BiSolidGroup, BiSolidTv, BiSolidUser } from 'react-icons/bi'
 
 const DDashboardSidebar = () => {
+    const responseHttp = useQuery({
+        queryFn: (token) => profileHttp(token),
+        onSuccess: (response) => {
+            console.log(response)
+            if (response.status == 200) {
+                return (response.data) 
+            }
+        },
+        onError: (error) => {
+            console.log(error)
+        },
+    })
     return (
         <div className='bg-primary-950 h-full text-white px-4 py-10 min-w-[250px]'>
             <ul>
+                <li className='bg-primary-50 bg-opacity-50 p-3 rounded-lg mb-8'>
+                    <div>name</div>
+                    <div>lastname</div>
+                </li>
                 <li className='flex gap-3 items-center mb-6 text-white bg-primary-200 bg-opacity-50 p-3 rounded-lg'>
                     <BiSolidDashboard className='text-2xl' />
                     <span className='text-lg leading-3 mt-1'>Dashboard</span>
