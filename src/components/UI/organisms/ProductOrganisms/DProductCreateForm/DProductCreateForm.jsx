c32d5f057ddd42b2908ac0ff1cb3882a46973a27
fatemeh <babaei.f1996@gmail.com>
2023-12-12 13:08:39 +0330

    feat: add form product create

--- src/components/UI/organisms/ProductOrganisms/DProductCreateForm/DProductCreateForm.jsx
+++ src/components/UI/organisms/ProductOrganisms/DProductCreateForm/DProductCreateForm.jsx
@@ -2,35 +2,18 @@
 
 import { Controller, useForm } from 'react-hook-form'
 import { useMutation } from 'react-query'
-import { Link,RichTextEditor } from '@mantine/tiptap'
-import { yupResolver } from '@hookform/resolvers/yup'
-import Highlight from '@tiptap/extension-highlight'
-import SubScript from '@tiptap/extension-subscript'
-import Superscript from '@tiptap/extension-superscript'
-import TextAlign from '@tiptap/extension-text-align'
-import Underline from '@tiptap/extension-underline'
-import { useEditor } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
 
-import { DTextInput } from '@components/UI/atoms/client'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+
+import { DButton, DTextArea, DTextInput } from '@components/UI/atoms/client'
 import { DInputField } from '@components/UI/molecules/client'
 
 import { createProductHttp } from '@core/services/api'
 import { profileValidation } from '@core/utils'
 
 const DProductCreateForm = () => {
-    const editor = useEditor({
-        extensions: [
-            StarterKit,
-            Underline,
-            Link,
-            Superscript,
-            SubScript,
-            Highlight,
-            TextAlign.configure({ types: ['heading', 'paragraph'] }),
-        ],
-        content,
-    })
+    
 
     const {
         handleSubmit,
@@ -78,52 +61,21 @@ const DProductCreateForm = () => {
                             render={({ field }) => <DTextInput label={'Category'} withAsterisk {...field} />}
                         />
                     </DInputField>
-                    <div>
-                        <RichTextEditor editor={editor}>
-                            <RichTextEditor.Toolbar sticky stickyOffset={60}>
-                                <RichTextEditor.ControlsGroup>
-                                    <RichTextEditor.Bold />
-                                    <RichTextEditor.Italic />
-                                    <RichTextEditor.Underline />
-                                    <RichTextEditor.Strikethrough />
-                                    <RichTextEditor.ClearFormatting />
-                                    <RichTextEditor.Highlight />
-                                    <RichTextEditor.Code />
-                                </RichTextEditor.ControlsGroup>
-
-                                <RichTextEditor.ControlsGroup>
-                                    <RichTextEditor.H1 />
-                                    <RichTextEditor.H2 />
-                                    <RichTextEditor.H3 />
-                                    <RichTextEditor.H4 />
-                                </RichTextEditor.ControlsGroup>
-
-                                <RichTextEditor.ControlsGroup>
-                                    <RichTextEditor.Blockquote />
-                                    <RichTextEditor.Hr />
-                                    <RichTextEditor.BulletList />
-                                    <RichTextEditor.OrderedList />
-                                    <RichTextEditor.Subscript />
-                                    <RichTextEditor.Superscript />
-                                </RichTextEditor.ControlsGroup>
-
-                                <RichTextEditor.ControlsGroup>
-                                    <RichTextEditor.Link />
-                                    <RichTextEditor.Unlink />
-                                </RichTextEditor.ControlsGroup>
-
-                                <RichTextEditor.ControlsGroup>
-                                    <RichTextEditor.AlignLeft />
-                                    <RichTextEditor.AlignCenter />
-                                    <RichTextEditor.AlignJustify />
-                                    <RichTextEditor.AlignRight />
-                                </RichTextEditor.ControlsGroup>
-                            </RichTextEditor.Toolbar>
-
-                            <RichTextEditor.Content />
-                        </RichTextEditor>
-                    </div>
+                    <DInputField className='col-span-2' errors={errors} fieldName={'description'}>
+                        <Controller
+                            name={'description'}
+                            control={control}
+                            render={({ field }) => <DTextArea label={'Description'} withAsterisk {...field} />}
+                        />
+                    </DInputField>
                 </div>
+                    <DButton
+                    type="submit"
+                    loading={isLoading}
+                    className="mt-4 bg-slate-900 w-full text-white h-[45px] max-w-[150px]"
+                >
+                    create
+                </DButton>
             </form>
         </div>
     )
